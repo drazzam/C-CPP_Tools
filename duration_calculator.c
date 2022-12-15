6999e23a7294d00820545ed605e276fc10bb7cfc
Ahmed Y. Azzam <109034951+drazzam@users.noreply.github.com>
2022-12-15 21:13:42 +0200

    Update duration_calculator.c

--- duration_calculator.c
+++ duration_calculator.c
@@ -1,31 +1,62 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 // Made By Ahmed Y. Azzam!
 // Enjoy It!
 
-int main (void){
-  while(1){
-  int day_of_year_1, month_of_year_1, year_1;
-  printf("\nEnter The Day: ");
-  scanf("%d", &day_of_year_1);
-  printf("Enter The Month: ");
-  scanf("%d", &month_of_year_1);
-  printf("Enter The Year: ");
-  scanf("%d", &year_1);
-  int day_of_year_2, month_of_year_2, year_2;
-  printf("Enter The Day: ");
-  scanf("%d", &day_of_year_2);
-  printf("Enter The Month: ");
-  scanf("%d", &month_of_year_2);
-  printf("Enter The Year: ");
-  scanf("%d", &year_2);
-  int calc_day, calc_month, year_calc, total_days;
-  year_calc= (year_2-year_1);
-  calc_day= abs(day_of_year_2-day_of_year_1);
-  calc_month= abs(month_of_year_2-month_of_year_1);
-  total_days= (year_calc*365)+(calc_month*30)+(calc_day);
-  printf("-----------------------\n Years: %d \n Months: %d \n Days: %d \n", year_calc, calc_month, calc_day);
-  printf(" The Total Number of Days Is: %d\n-----------------------", total_days);
-    }
+/*
+ * This function calculates the number of years, months, and days
+ * between two dates.
+ *
+ * @param year1 The year of the first date
+ * @param month1 The month of the first date
+ * @param day1 The day of the first date
+ * @param year2 The year of the second date
+ * @param month2 The month of the second date
+ * @param day2 The day of the second date
+ * @param years The number of years between the two dates (output)
+ * @param months The number of months between the two dates (output)
+ * @param days The number of days between the two dates (output)
+ */
+void date_diff(int year1, int month1, int day1, int year2, int month2, int day2, int *years, int *months, int *days) {
+  // Calculate the number of years
+  *years = year2 - year1;
+
+  // Calculate the number of months
+  *months = month2 - month1;
+  if (*months < 0) {
+    // If the number of months is negative, reduce the number of years by 1
+    // and add 12 months to the number of months
+    *years -= 1;
+    *months += 12;
   }
+
+  // Calculate the number of days
+  *days = day2 - day1;
+  if (*days < 0) {
+    // If the number of days is negative, reduce the number of months by 1
+    // and add the number of days in the previous month to the number of days
+    *months -= 1;
+    *days += 30; // Assume all months have 30 days for simplicity
+  }
+}
+
+int main() {
+  int year1, month1, day1, year2, month2, day2;
+  int years, months, days;
+
+  // Prompt the user for the first date
+  printf("Enter The First Date (YYYY MM DD): ");
+  scanf("%d %d %d", &year1, &month1, &day1);
+
+  // Prompt the user for the second date
+  printf("Enter the second date (YYYY MM DD): ");
+  scanf("%d %d %d", &year2, &month2, &day2);
+
+  // Calculate the number of years, months, and days between the two dates
+  date_diff(year1, month1, day1, year2, month2, day2, &years, &months, &days);
+
+  // Print the result
+  printf("\nThe Duration Between The Two Dates: \n");
+  printf("-----------------\n Years: %d\n Months: %d\n Days: %d\n-----------------\n", years, months, days);
+
+  return 0;
+}
